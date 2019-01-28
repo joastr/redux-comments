5a4938834cd7a4d8f84a65f1a73bdcc68bc0a819
Joanna Kasperek <joastr1@gmail.com>
2019-01-28 12:55:28 +0100

    initial commit

---
+++ src/actions.js
@@ -0,0 +1,33 @@
+import uuid from'uuid';
+
+export const ADD_COMMENT ='ADD_COMMENT';
+export const THUMB_UP_COMMENT ='THUMB_UP_COMMENT';
+
+
+
+
+function addComment(text) {
+  return {
+    type: ADD_COMMENT,
+    id: uuid.v4(),
+    text: text
+  }
+}
+
+export addComment
+
+
+
+// kreator akcji:
+function thumbUpComment(commentId) {
+  return {
+    type: THUMB_UP_COMMENT,
+    id: commentId
+  }
+}
+
+export thumbUpComment
+
+
+
+
